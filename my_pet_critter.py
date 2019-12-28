825dedee763b37fc7fc1cb93c6f625e1c64364aa
kgl-magnificent <58805748+kgl-magnificent@users.noreply.github.com>
2019-12-28 13:48:06 +0300

    Create my_pet_critter.py

---
+++ my_pet_critter.py
@@ -0,0 +1,70 @@
+#Моя зверушка
+class Critter(object):
+    def __init__(self, name, hunger = 0, boredom = 0):
+        self.name = name
+        self.hunger = hunger
+        self.boredom = boredom
+    def __pass_time(self): #закрытый метод использутся только во время вызова других методов
+        self.hunger += 1
+        self.boredom += 1
+    @property
+    def mood(self):
+        unhappiness = self.boredom + self.hunger
+        if unhappiness < 5:
+            m = "good"
+        if 5 <= unhappiness <= 10:
+            m = "not bad"
+        else:
+            m = "awful"
+        return m
+    def talk(self):
+        print("Меня зовут", self.name, "и сейчас я чувствую себя", self.mood)
+    def eat(self, food = 4):
+        print("Thanks")
+        self.hunger -= food
+        if self.hunger < 0:
+            self.hunger = 0
+        self.__pass_time()
+def main():
+    crit_name = input("ВВедите название зверушки")
+    crit = Critter(crit_name)
+    choice = None
+    while choice != "0":
+        print \
+            ("""
+            Critter Caretaker
+
+            0 - Quit
+            1 - Listen to your critter
+            2 - Feed your critter
+            3 - Play with your critter
+            """)
+
+        choice = input("Choice: ")
+        print()
+
+        # exit
+        if choice == "0":
+            print("Good-bye.")
+
+        # listen to your critter
+        elif choice == "1":
+            crit.talk()
+
+        # feed your critter
+        elif choice == "2":
+            crit.eat()
+
+        # play with your critter
+        elif choice == "3":
+            crit.play()
+
+        # some unknown choice
+        else:
+            print("\nSorry, but", choice, "isn't a valid choice.")
+
+main()
+
+
+
+
